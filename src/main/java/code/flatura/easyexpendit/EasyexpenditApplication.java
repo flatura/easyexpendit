851675cf08246c8fe184154abaa40156c6337a24
Dmitry Morozov <flatura@gmail.com>
2021-10-05 19:06:31 +0300

    Initial commit. Spring Boot 2.5.4. Spring Web, Spring Test, Maven plugin

---
+++ src/main/java/code/flatura/easyexpendit/EasyexpenditApplication.java
@@ -0,0 +1,13 @@
+package code.flatura.easyexpendit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyexpenditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyexpenditApplication.class, args);
+    }
+
+}
---
+++ src/test/java/code/flatura/easyexpendit/EasyexpenditApplicationTests.java
@@ -0,0 +1,13 @@
+package code.flatura.easyexpendit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyexpenditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
